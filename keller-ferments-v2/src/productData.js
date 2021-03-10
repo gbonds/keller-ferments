6d6f4ef1c5a6a3b68b037f77d35d57ea322d19c1
gbonds <ge.bonds@gmail.com>
2021-03-10 16:52:24 -0500

    Created data sets for products and recipes

---
+++ keller-ferments-v2/src/productData.js
@@ -0,0 +1,52 @@
+const products = [
+    {
+        id: 1,
+        name: 'Classic Pickles',
+        price: 8,
+        currency: 'USD',
+        image:
+            './img/pickles-thumbnail_800px.jpg',
+        description:
+            'Lorem ipsum dolor sit, amet consectetur adipisicing.',
+        category: 'food product',
+        favorite: true
+    },
+    {
+        id: 2,
+        name: 'Kombucha Starter Kit',
+        price: 35,
+        currency: 'USD',
+        image:
+            './img/pickles-thumbnail_800px.jpg',
+        description:
+            'Lorem ipsum dolor sit, amet consectetur adipisicing.',
+        category: 'home kits',
+        favorite: true
+    },
+    {
+        id: 3,
+        name: "Yo Mama's Kimchi",
+        price: 12,
+        currency: 'USD',
+        image:
+            './img/pickles-thumbnail_800px.jpg',
+        description:
+            'Lorem ipsum dolor sit, amet consectetur adipisicing.',
+        category: 'food product',
+        favorite: true
+    },
+    {
+        id: 4,
+        name: "Keller Ferments Gift Pack",
+        price: 40,
+        currency: 'USD',
+        image:
+            './img/pickles-thumbnail_800px.jpg',
+        description:
+            'Lorem ipsum dolor sit, amet consectetur adipisicing.',
+        category: 'gift package',
+        favorite: false
+    },
+];
+
+export default products;
---
+++ keller-ferments-v2/src/recipeData.js
@@ -0,0 +1,37 @@
+const recipes = [
+    {
+        id: 1,
+        name: 'Kimchi Pancake',
+        postDate: '2020-06-12',
+        image:
+            './img/pickles-thumbnail_800px.jpg',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate modi odio, voluptatibus dignissimos vel qui repellat, distinctio culpa autem optio iure quae accusamus. Quos, exercitationem?',
+        ingredients: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam accusantium laudantium cumque?',
+        instructions: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid, repellat eos. A autem tempore dicta itaque nemo maiores sequi vitae rerum nam, consequatur sapiente corrupti nihil minima in sed rem, quis minus incidunt perspiciatis quos provident atque perferendis consequuntur ad!'
+    },
+    {
+        id: 2,
+        name: 'Party Snack Board',
+        postDate: '2020-03-25',
+        image:
+            './img/pickles-thumbnail_800px.jpg',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate modi odio, voluptatibus dignissimos vel qui repellat, distinctio culpa autem optio iure quae accusamus. Quos, exercitationem?',
+        ingredients: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam accusantium laudantium cumque?',
+        instructions: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid, repellat eos. A autem tempore dicta itaque nemo maiores sequi vitae rerum nam, consequatur sapiente corrupti nihil minima in sed rem, quis minus incidunt perspiciatis quos provident atque perferendis consequuntur ad!'
+    },
+    {
+        id: 3,
+        name: 'Rueben Sandwich',
+        postDate: '2020-02-14',
+        image:
+            './img/pickles-thumbnail_800px.jpg',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate modi odio, voluptatibus dignissimos vel qui repellat, distinctio culpa autem optio iure quae accusamus. Quos, exercitationem?',
+        ingredients: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam accusantium laudantium cumque?',
+        instructions: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid, repellat eos. A autem tempore dicta itaque nemo maiores sequi vitae rerum nam, consequatur sapiente corrupti nihil minima in sed rem, quis minus incidunt perspiciatis quos provident atque perferendis consequuntur ad!'
+    },
+];
+
+export default recipes;
