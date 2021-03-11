a92bac733a266efce2585254857acfa34ea545e8
gbonds <ge.bonds@gmail.com>
2021-03-11 13:50:43 -0500

    Updated favorite array to only include first 3 in array

--- keller-ferments-v2/src/Product.js
+++ keller-ferments-v2/src/Product.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import products from './productData'
 
-const favProdList = products.filter(singleProduct => singleProduct.favorite === true)
+/* Filters all products with favorite property set to true, then filters first 3 in the array (0 to 2 in the index) */
+const allFavProdList = products.filter(singleProduct => singleProduct.favorite === true)
+const topFavProdList = allFavProdList.slice(0, 3)
 
 export default function Product() {
     return (
         <>
-            {favProdList.map((singleProduct) => {
+            {topFavProdList.map((singleProduct) => {
                 const { id, url, name, price, image } = singleProduct
 
                 return (
