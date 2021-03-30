ee03ed56a1526e099f2711d47584451198756540
gbonds <ge.bonds@gmail.com>
2021-03-30 13:41:49 -0400

    Added hide/show functionality to header nav

--- keller-ferments-v2/src/Header.js
+++ keller-ferments-v2/src/Header.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 // TODO add logo import here, was having issues with file path in production. -GB 3/30/21
+import { FaBars } from 'react-icons/fa'
 
 export default function Header() {
+    const [showLinks, setShowLinks] = useState(false)
+
     return (
         <header className="header-section">
             <a href="index.html" className="header__logo-link">
@@ -11,16 +14,26 @@ export default function Header() {
                     className="header__logo"
                 />
             </a>
-            <nav className="header__nav">
-                <ul>
-                    <li><a href="">Home</a></li>
-                    <li><a href="">Products</a></li>
-                    <li><a href="">Locations</a></li>
-                    <li><a href="">About</a></li>
-                    <li><a href="">Recipes</a></li>
-                    <li><a href="">Contact</a></li>
-                </ul>
-            </nav>
-        </header>
+            <button
+                className="nav-toggle"
+                onClick={() => {
+                    setShowLinks(!showLinks)
+                }}
+            >
+                <FaBars />
+            </button>
+            {showLinks &&
+                <div className="header__nav show-container">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Products</a></li>
+                        <li><a href="#">Locations</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Recipes</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+            }
+        </header >
     )
 }
