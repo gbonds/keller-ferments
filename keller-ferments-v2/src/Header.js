0a970648df67c809ada68e09263d09a7d12c2e7f
gbonds <ge.bonds@gmail.com>
2021-03-30 17:30:39 -0400

    Built out animation for nav toggle with useEffect and useRef

--- keller-ferments-v2/src/Header.js
+++ keller-ferments-v2/src/Header.js
@@ -1,9 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 // TODO add logo import here, was having issues with file path in production. -GB 3/30/21
 import { FaBars } from 'react-icons/fa'
 
 export default function Header() {
-    const [showLinks, setShowLinks] = useState(true)
+    const [showLinks, setShowLinks] = useState(false)
+    const navLinksContainerRef = useRef(null)
+    const navLinksRef = useRef(null)
+
+    useEffect(() => {
+        const linksHeight = navLinksRef.current.getBoundingClientRect().height // returns values of element's height, can also find width, xy position
+        if (showLinks) {
+            navLinksContainerRef.current.style.height = `${linksHeight}px`
+            // if showLinks is true, then show links
+        } else {
+            navLinksContainerRef.current.style.height = '0px'
+            // if showLinks is false, then make element 0px high
+        }
+    }, [showLinks]) // everytime showLinks changes, do above callback function
 
     return (
         <header className="header-section">
@@ -25,18 +38,17 @@ export default function Header() {
                 </button>
             </div>
 
-            {showLinks &&
-                <div className="header__nav-links show-container">
-                    <ul>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Products</a></li>
-                        <li><a href="#">Locations</a></li>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Recipes</a></li>
-                        <li><a href="#">Contact</a></li>
-                    </ul>
-                </div>
-            }
+            <div className='header__nav-links-container' ref={navLinksContainerRef} >
+                <ul className='header__nav-links' ref={navLinksRef} >
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Products</a></li>
+                    <li><a href="#">Locations</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Recipes</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+
         </header >
     )
 }
