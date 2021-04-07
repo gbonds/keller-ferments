0f9ba1aea18746fb4b0fbc80579600248ba488bb
gbonds <ge.bonds@gmail.com>
2021-04-07 11:26:44 -0400

    Added links to url field in recipe data

--- keller-ferments-v2/src/recipeData.js
+++ keller-ferments-v2/src/recipeData.js
@@ -3,6 +3,7 @@ const recipes = [
         id: 1,
         name: 'Party Snack Board',
         date: 'March 24, 2020',
+        url: 'https://www.google.com/search?q=party+snack+board+recipe&rlz=1C5CHFA_enUS898US898&ei=4s5tYN_sE4yk_QbXtZXQDw&oq=party+snack+board+recipe&gs_lcp=Cgdnd3Mtd2l6EAMyCAghEBYQHRAeMggIIRAWEB0QHjIICCEQFhAdEB4yCAghEBYQHRAeOggILhCRAhCTAjoECAAQQzoECC4QQzoFCC4QkQI6BQgAEJECOgsILhCxAxDHARCjAjoICAAQsQMQgwE6DgguELEDEMcBEKMCEJMCOgUIABDJAzoFCAAQkgM6CAgAELEDEMkDOgUIABCxAzoCCAA6BggAEBYQHlDmeVjWjwFg6JABaABwAXgAgAGoAYgBpxGSAQQyMC41mAEAoAEBqgEHZ3dzLXdpesABAQ&sclient=gws-wiz&ved=0ahUKEwjf5bPQuOzvAhUMUt8KHddaBfoQ4dUDCA0&uact=5',
         image:
             'img/charcuterie-board_1000px.jpg',
         description:
@@ -14,6 +15,7 @@ const recipes = [
         id: 2,
         name: 'Kimchi Pancake',
         date: 'June 12, 2020',
+        url: 'https://www.google.com/search?q=kimchi+pancake+reipce&rlz=1C5CHFA_enUS898US898&oq=kimchi+pancake+reipce&aqs=chrome..69i57j0i13l4.2897j0j7&sourceid=chrome&ie=UTF-8',
         image:
             'img/kimchi-pancake_1000px.jpg',
         description:
@@ -25,6 +27,7 @@ const recipes = [
         id: 3,
         name: 'Rueben Sandwich',
         date: 'February 14, 2021',
+        url: 'https://www.google.com/search?q=reuben+sandwich+recipe&rlz=1C5CHFA_enUS898US898&ei=9s5tYJT1C4Sb_Qbh0rDIBg&oq=reuben+sandwich+recipe&gs_lcp=Cgdnd3Mtd2l6EAMyBQgAELEDMgIIADICCAAyAggAOgcIABBHELADOgUIABCRAjoLCC4QsQMQxwEQowI6BAgAEEM6BQguELEDOggILhCxAxCTAjoHCAAQsQMQQzoFCAAQyQM6BQgAEJIDOgYIABAWEB5QklxYqXhgynloAnACeAGAAYACiAGPEZIBBjIwLjMuMZgBAKABAaoBB2d3cy13aXrIAQjAAQE&sclient=gws-wiz&ved=0ahUKEwiUyPDZuOzvAhWETd8KHWEpDGkQ4dUDCA0&uact=5',
         image:
             'img/reuben-sandwich_1000px.jpg',
         description:
