66c0cc9efc3031cfc26cb3d16f293792b7cb942b
gbonds <ge.bonds@gmail.com>
2021-04-12 14:17:12 -0400

    Deleted folder and files for previous newsletter signup components. Active component is now called EmailSignup.js

--- keller-ferments-v2/src/newsletter/NewsletterFormSuccessBlock.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-export default ({email}) => {
-  return (
-      <div className="email-input__success-msg">Success! We've added <strong>{email}</strong> to our newsletter. Thanks!</div>
-  );
-};
