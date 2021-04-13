a5b80f8a503ef8f4c10bfe6ce2afd4ba8a812b00
gbonds <ge.bonds@gmail.com>
2021-04-13 15:55:43 -0400

    Installed new Create React App, added my custom src files back into src folder

---
+++ src/productData.js
@@ -0,0 +1,56 @@
+const products = [
+    {
+        id: 1,
+        name: 'Classic Pickles',
+        price: 8,
+        currency: 'USD',
+        image:
+            'img/pickles-thumbnail_800px.jpg',
+        description:
+            'Lorem ipsum dolor sit, amet consectetur adipisicing.',
+        category: 'food product',
+        favorite: true,
+        url: 'https://www.google.com/search?ei=yFpKYI7PGsHH5gKK95yAAw&q=pickles&oq=pickles&gs_lcp=Cgdnd3Mtd2l6EAMyCgguELEDEEMQkwIyBAgAEEMyBAgAEEMyCAgAELEDEIMBOgUIABCxAzoLCC4QsQMQxwEQowI6AggAOgcILhBDEJMCOgUILhCxA1DZgwFYlYkBYP6JAWgAcAJ4AIABnAGIAYcGkgEDMi41mAEAoAEBqgEHZ3dzLXdpesABAQ&sclient=gws-wiz&ved=0ahUKEwiO0_P96KjvAhXBo1kKHYo7BzAQ4dUDCA0&uact=5'
+    },
+    {
+        id: 2,
+        name: 'Kombucha Starter Kit',
+        price: 35,
+        currency: 'USD',
+        image:
+            'img/kombucha-thumbnail_800px.jpg',
+        description:
+            'Lorem ipsum dolor sit, amet consectetur adipisicing.',
+        category: 'home kits',
+        favorite: true,
+        url: 'https://www.google.com/search?source=hp&ei=vlpKYMznN6Tr5gKVn6SgCg&iflsig=AINFCbYAAAAAYEpozmcmcDu5rl0RJpzFObM5h27Lzs6L&q=kombucha&oq=kombucha&gs_lcp=Cgdnd3Mtd2l6EAMyCAguELEDEJMCMgUILhCxAzIFCAAQsQMyBQgAELEDOgsILhCxAxDHARCjAjoOCC4QsQMQxwEQowIQkwI6AggAOgsILhCxAxDHARCvAVCLPFjEQmCZQ2gBcAB4AIABeYgB-gSSAQM1LjKYAQCgAQGqAQdnd3Mtd2l6sAEA&sclient=gws-wiz&ved=0ahUKEwjMvq756KjvAhWktVkKHZUPCaQQ4dUDCAk&uact=5'
+    },
+    {
+        id: 3,
+        name: "Yo Mama's Kimchi",
+        price: 12,
+        currency: 'USD',
+        image:
+            'img/kimchi-thumbnail_800px.jpg',
+        description:
+            'Lorem ipsum dolor sit, amet consectetur adipisicing.',
+        category: 'food product',
+        favorite: true,
+        url: 'https://www.google.com/search?ei=21pKYJ22Hoy15gLb1LrwBQ&q=kimchi&oq=kimchi&gs_lcp=Cgdnd3Mtd2l6EAMyCgguELEDEEMQkwIyBAgAEEMyBwgAELEDEEMyBQguELEDOg4ILhCxAxDHARCjAhCTAjoECC4QQzoNCC4QxwEQrwEQQxCTAlDiXljgYmCRZGgAcAJ4AIABiAGIAb8GkgEDMC43mAEAoAEBqgEHZ3dzLXdpesABAQ&sclient=gws-wiz&ved=0ahUKEwjdj_-G6ajvAhWMmlkKHVuqDl4Q4dUDCA0&uact=5'
+    },
+    {
+        id: 4,
+        name: "Keller Ferments Gift Pack",
+        price: 40,
+        currency: 'USD',
+        image:
+            'img/gift-thumbnail_800px.jpg',
+        description:
+            'Lorem ipsum dolor sit, amet consectetur adipisicing.',
+        category: 'gift package',
+        favorite: false,
+        url: 'https://www.google.com/search?ei=6VpKYN6XLsGe5gKK44ugAg&q=gift+pack&oq=gift+pack&gs_lcp=Cgdnd3Mtd2l6EAMyAggAMgIIADICCAAyAggAOgcILhBDEJMCOgQILhBDOggILhDHARCjAjoICAAQsQMQgwE6BAgAEEM6BQgAELEDUPZUWL9fYORmaABwAngAgAGyAYgB2geSAQM3LjOYAQCgAQGqAQdnd3Mtd2l6wAEB&sclient=gws-wiz&ved=0ahUKEwiesOWN6ajvAhVBj1kKHYrxAiQQ4dUDCA0&uact=5'
+    },
+];
+
+export default products;
